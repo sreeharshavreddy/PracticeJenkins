1ce60e33d74a4d7804734d01f470c847f5209820
ADMIN <ADMIN@DESKTOP-7CF6N7D>
2022-03-16 12:24:34 +0530

    comit

--- src/test/java/ContactTest/Jenkins.java
+++ src/test/java/ContactTest/Jenkins.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Jenkins
 {
   @Test
@@ -15,10 +17,12 @@ public class Jenkins
 	  WebDriver driver = null;
 		if(Browser.equalsIgnoreCase("chrome"))
 		{
+			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
 		}
 		else if(Browser.equalsIgnoreCase("firefox"))
 		{
+			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
 		}
 		driver.get("http://localhost:8888");
